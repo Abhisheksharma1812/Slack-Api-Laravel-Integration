2dfa64a7b9f3875bf720a43506d66f710aec8f7e
Abhisheksharma1812 <110279711+Abhisheksharma1812@users.noreply.github.com>
2024-10-30 12:07:45 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 I make these routes in web file for authenticate user and getting token for adding app in employee workplace.
- 
+
+<?php
+use App\Http\Controllers\MessageController;
+
 Route::get('/authenticate', [MessageController::class, 'userAuthenticate']);
 Route::get('/slack-callback', [MessageController::class, 'handleSlackCallback']);
